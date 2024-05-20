cb1b34757c8515ed5929cc4d51d75ffea33abd8d
Michael <oliengo.michael@gmail.com>
2024-05-20 15:10:30 +0300

    complete react hook lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>React empowers developers to build dynamic and responsive user interfaces with the speed of a framework and the flexibility of a library.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
